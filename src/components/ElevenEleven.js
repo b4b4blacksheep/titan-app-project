10693ba96d32c9c176a17463c2d4741f94d82815
b4b4blacksheep <babablacksheep122333444455555@gmail.com>
2023-11-10 11:40:17 +0800

    updated eleven-eleven landing sm

--- src/components/ElevenEleven.js
+++ src/components/ElevenEleven.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
-
-// Assets
 import VidLanding1 from '../assets/landing/app-titan-eleven-eleven.mp4';
+import VidLanding2 from '../assets/landing/app-titan-eleven-eleven-sm.mp4';
 import '../assets/landing/styles.css';
 
-export default function elevenEleven () {
+export default function ElevenEleven() {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    // Attach the event listener
+    window.addEventListener('resize', handleResize);
+
+    // Detach the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <Link to="/collections/adidas-basketball-chapter-03">
       <div className="py-2">
         <div className="video-container">
-          <video className="full-screen-video" autoPlay loop muted>
-  <source src={VidLanding1} type="video/mp4" />
-</video>
+          {isSmallScreen ? (
+            <video className="full-screen-video" autoPlay loop muted>
+              <source src={VidLanding2} type="video/mp4" />
+            </video>
+          ) : (
+            <video className="full-screen-video" autoPlay loop muted>
+              <source src={VidLanding1} type="video/mp4" />
+            </video>
+          )}
         </div>
       </div>
     </Link>
   );
-};
+}
